4485b5abc59a0aadd2aace78264a655d5fbe5e33
Leon <xo6u123@gmail.com>
2023-04-18 14:18:47 +0800

    Update for lab 2

--- Lab 2/main.js
+++ Lab 2/main.js
@@ -4,6 +4,7 @@ $(function(){
     //一秒鐘有1000毫秒//每分鐘60秒、每小時60分鐘、每天24小時
     let millisecsPerDay = 24*60*60*1000;
     let Arr= new Array;
+    var x=0;
     $("#moviedate").change(function(){
         
         $("#moviedate").attr("value",$(this).val()); //賦值
@@ -18,7 +19,7 @@ $(function(){
         startDate.setSeconds(0);
         //alert(Arr[1]);
     
-    for(var x=0;x<topicCount;x++){
+    for(;x<topicCount;x++){
         $("#courseTable").append("<tr>"+
         `<td>${x+1}</td>`+
         `<td>${(new Date(startDate.getTime()+7*x*millisecsPerDay)).toLocaleDateString()}</td>`+
@@ -30,5 +31,18 @@ $(function(){
     
     });
     
-    
+    var testInput = document.getElementById("testInput");
+    var submitBtn = document.querySelector(".submitBtn");
+    function FsubmitBtn(value) {
+        var str = "";
+        var submitValue = testInput.value;
+        str = submitValue;
+        $("#courseTable").append("<tr>"+
+        `<td>${(x++)+1}</td>`+
+        `<td>${(new Date(startDate.getTime()+7*(x-1)*millisecsPerDay)).toLocaleDateString()}</td>`+
+        `<td>${str}</td>`+
+        "</tr>");
+        
+    }
+    submitBtn.addEventListener("click", FsubmitBtn);
 });
--- Lab 2/topic.js
+++ Lab 2/topic.js
@@ -4,9 +4,9 @@ var topic = [
     "國定假日",
     "Lab 2 & Lab 3",
     "Lab 4",
-    "Lab 5"
+    "Lab 5",
+    
 ];
-
 
 
 
